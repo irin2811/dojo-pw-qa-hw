f45e2164479ca0098f63f2fc89b5673144bb000d
Iryna <iryna.zaporozhets@raccoongang.com>
2026-09-20 11:06:04 +0300

    add changes for tests independence

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,60 +1,47 @@
 import { test, expect } from '@playwright/test';
 
-const uniqueEmail = `student-${Date.now()}-${Math.random()}@example.com`;
-const uniqueUsername = `student-${Date.now()}`;
+const userPassword = `password123`;
 
-const userName = 'iryna123';
-const userEmail = 'iryna123@example.com';
-const userPassword = 'password123';
-
-//const userName = uniqueUsername;
-//const userEmail = uniqueEmail;
-//const userPassword = `password123`;
+async function userRegistration(page: any, userName: string, userEmail: string, userPassword: string) {
+  await page.goto('/register');
+  await page.getByTestId('auth-username').fill(userName);
+  await page.getByTestId('auth-email').fill(userEmail);
+  await page.getByTestId('auth-password').fill(userPassword);
+  await page.getByTestId('register-confirm-password').fill(userPassword);
+  await page.getByTestId('register-terms').check();
+  await page.getByRole('button', { name: 'Create account' }).click();
+}
 
 test.describe('Registration', { tag: '@auth' }, () => {
+  let uniqueEmail: string;
+  let uniqueUserName: string;
 
   test.beforeEach(async ({ page, context }) => {
+    uniqueEmail = `student-${Date.now()}-${Math.random()}@example.com`;
+    uniqueUserName = `student-${Date.now()}`;
+
     await context.clearCookies();
     await page.goto('/register');
   });
 
   test('user registers with valid data', async ({ page }) => {
-    await page.getByTestId('auth-username').fill(uniqueUsername);
-    await page.getByTestId('auth-email').fill(uniqueEmail);
-    await page.getByTestId('auth-password').fill(userPassword);
-    await page.getByTestId('register-confirm-password').fill(userPassword);
-    await page.getByTestId('register-terms').check();
-
-    await expect(page.getByRole('button', { name: 'Create account' })).toBeVisible();
-    await page.getByRole('button', { name: 'Create account' }).click();
-
-    await expect(page.getByTestId('article-list-empty')).toContainText('No articles here yet.');
-    await expect(page.getByTestId('nav-home')).toContainText('Home');
-    await expect(page.getByTestId('nav-new-article')).toContainText('New article');
-    await expect(page.getByTestId('nav-profile')).toContainText(uniqueUsername);
-    await page.getByTestId('nav-profile').click();
-    await expect(page.getByTestId('profile-username')).toContainText(uniqueUsername);
+    await userRegistration(page, uniqueUserName, uniqueEmail, userPassword);
+    await expect(page.getByTestId('nav-profile')).toContainText(uniqueUserName);
   });
 
-  test('user registers with invalid data (existing email)', async ({ page }) => {
-    await page.getByTestId('auth-username').fill(userName);
-    await page.getByTestId('auth-email').fill(userEmail);
-    await page.getByTestId('auth-password').fill(userPassword);
-    await page.getByTestId('register-confirm-password').fill(userPassword);
-    await page.getByTestId('register-terms').check();
-    await page.getByRole('button', { name: 'Create account' }).click();
+  test('user registers with invalid data (existing email)', async ({ page, context }) => {
+    await userRegistration(page, uniqueUserName, uniqueEmail, userPassword);
+    await expect(page.getByTestId('nav-profile')).toContainText(uniqueUserName);
 
+    await context.clearCookies();
+    await page.goto('/register');
+
+    await userRegistration(page, uniqueUserName, uniqueEmail, userPassword);
     await expect(page.getByTestId('error-messages')).toContainText('body email або username вже зайняті');
   });
 
   test('user registers with invalid data (empty fields)', async ({ page }) => {
-    await page.getByTestId('auth-username').fill('');
-    await page.getByTestId('auth-email').fill('');
-    await page.getByTestId('auth-password').fill('');
-    await page.getByTestId('register-confirm-password').fill('');
-    await page.getByTestId('register-terms').check();
-    await expect(page.getByTestId('auth-submit')).toContainText('Create account');
-    await page.getByTestId('auth-submit').click();
+    await userRegistration(page, '', '', '');
 
     await expect(page.getByTestId('error-messages')).toContainText('username ім\'я має містити щонайменше 3 символи');
     await expect(page.getByTestId('error-messages')).toContainText('email некоректний email');
@@ -63,7 +50,16 @@ test.describe('Registration', { tag: '@auth' }, () => {
 });
 
 test.describe('Login', { tag: '@auth' }, () => {
+  let userEmail: string;
+  let userName: string;
+
   test.beforeEach(async ({ page, context }) => {
+    userEmail = `student-${Date.now()}-${Math.random()}@example.com`;
+    userName = `student-${Date.now()}`;
+
+    await context.clearCookies();
+    await userRegistration(page, userName, userEmail, userPassword);
+
     await context.clearCookies();
     await page.goto('/login');
   });
